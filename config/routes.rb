4dffdab28e938f582412bfe29c9f47400a3bcde5
Jacqueline Quach <jcqlnquach@gmail.com>
2014-04-13 02:25:29 -0700

    Trying tto Lead to New Pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
+  get "createprofile" => "pages#createprofile" #creates createprofile_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
